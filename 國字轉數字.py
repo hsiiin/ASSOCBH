2282cce7dd4a4157a3e5b71eaf6635590243325e
deocoplay <115851226+deocoplay@users.noreply.github.com>
2022-12-14 18:14:13 +0800

    Update 國字轉數字.py

--- 國字轉數字.py
+++ 國字轉數字.py
@@ -1,11 +1,12 @@
 import pandas as pd
     
-df=pd.read_csv("./test.csv")
+df=pd.read_csv("./Judgement_Feature.csv")
     # print(df)
-df_content=df.loc[:,"judge_content"]
-    # print(df_content)
+df_content=df.loc[:,"judge_result"]
+print(df_content.head())
+
     
-num={"壹":1, "一":1, "貳":2 , "贰":2, "二":2, "參":3, "参":3, "叁":3, "叄":3, "三":3, "肆":4, "四":4,  "伍":5, "五":5, "陸":6, "六":6, "柒":7, "七":7, "捌":8, "八":8, "玖":9, "九":9, "拾":10,"十":10, "佰":100, "百":100}
+num={"壹":1, "一":1, "貳":2 , "贰":2, "二":2, "參":3, "参":3, "叁":3, "叄":3, "三":3, "肆":4, "四":4,  "伍":5, "五":5, "陸":6, "六":6, "柒":7, "七":7, "捌":8, "八":8, "玖":9, "九":9, "拾":10,"十":10, "佰":100, "百":100, "零":0}
 unit={"年":365, "月":30, "日":1}
     
 transfer=[]
@@ -34,6 +35,11 @@ for i in df_content:
             transfer2.append(number)
             transfer2.append(n)
             number=0
+        elif n ==100:
+            if number==0:
+                number+=100
+            else:
+                number*=100
         elif n !=10:
             number+=n
         elif n ==10:
@@ -41,11 +47,6 @@ for i in df_content:
                 number+=10
             else:
                 number*=10
-        elif n ==100:
-            if number==0:
-                number+=100
-            else:
-                number*=100
     # print(transfer2)
     # 將數字和單位做運算
     for m in transfer2:
@@ -59,8 +60,8 @@ for i in df_content:
     # 將刑期加總並加到csv中
     number=sum(transfer3)
     judge.append(number)
-        # print(judge)
+    # print(judge)
     
-df['number']=judge
+df['judge_resultInt_2']=judge
     # print(df)
-pd.DataFrame(df).to_csv("./test.csv", index=None, encoding="utf-8-sig")
+pd.DataFrame(df).to_csv("./Judgement_Feature.csv", index=None, encoding="utf-8-sig")
